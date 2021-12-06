01044bd5269136176318b18aabfbe6aa8f049714
Lucas Azevedo Souza <49877556+Lucasoluzera@users.noreply.github.com>
2021-12-05 22:42:36 -0300

    Cors

---
+++ src/main/java/com/example/toledoBank/api/seguranca/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.toledoBank.api.seguranca;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
--- src/main/java/com/example/toledoBank/api/seguranca/WebSecurityConfig.java
+++ src/main/java/com/example/toledoBank/api/seguranca/WebSecurityConfig.java
@@ -101,8 +101,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("**");
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("*");
         config.addAllowedHeader("**");
         config.addAllowedMethod(HttpMethod.GET);
         config.addAllowedMethod(HttpMethod.POST);
